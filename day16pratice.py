1b0dd7bf52bc6126bca902f33f9ede4d332e228e
SriramS2608 <srirams2608@gmail.com>
2026-03-06 22:19:15 +0530

    initial commit

---
+++ day16pratice.py
@@ -0,0 +1,25 @@
+#  reverse the number using while
+num=123456789
+rev=0
+while num>0:
+    digit=num%10
+    rev=rev*10+digit
+    num=num//10
+print(rev)
+
+
+
+#reverse palindrome numbers
+num=12345678987654321
+temp=num
+rev=0
+while temp>0:
+    digit=temp%10
+    rev=rev*10+digit
+    temp=temp//10
+if num==rev:
+    print(f'{num} is palinddrome')
+else:
+    print(f'{num} is not in palindrome')
+
+
